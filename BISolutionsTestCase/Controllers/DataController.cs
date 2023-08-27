8729a7d6019cf76b0681477c79715f4d9a2e63a2
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-27 23:25:37 +0300

    fix add Numbers

--- BISolutionsTestCase/Controllers/DataController.cs
+++ BISolutionsTestCase/Controllers/DataController.cs
@@ -11,11 +11,17 @@ namespace BISolutionsTestCase.Controllers
         public IActionResult SumOddNumbers([FromBody] int[] numbers)
         {
             int sum = 0;
-            for (int i = 1; i < numbers.Length; i += 2)
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
             {
+
                 if (numbers[i] % 2 != 0)
                 {
-                    sum += numbers[i];
+                    count++;
+                    if (count % 2 ==0)
+                    {
+                        sum += numbers[i];
+                    }   
                 }
             }
             return Ok(Math.Abs(sum));
