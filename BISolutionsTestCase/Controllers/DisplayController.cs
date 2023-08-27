c69af96b97f6b300d20cb20e90d00b13e876d58c
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-27 22:52:26 +0300

    Add DisplayController

---
+++ BISolutionsTestCase/Controllers/DisplayController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BISolutionsTestCase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DisplayController : ControllerBase
+    {
+        IHttpClientFactory _client;
+        public DisplayController(IHttpClientFactory client)
+        {
+            _client = client;
+        }
+
+    }
+}
