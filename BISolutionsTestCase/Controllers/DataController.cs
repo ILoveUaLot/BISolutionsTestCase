5a63350d04a8476d8ef4bb0bd4fa898e70c836b4
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-27 22:27:02 +0300

    Add method for sum odd numbers

--- BISolutionsTestCase/Controllers/DataController.cs
+++ BISolutionsTestCase/Controllers/DataController.cs
@@ -7,6 +7,18 @@ namespace BISolutionsTestCase.Controllers
     [ApiController]
     public class DataController : ControllerBase
     {
-        
+        [HttpPost("AddNumbers")]
+        public IActionResult SumOddNumbers([FromBody] int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 1; i < numbers.Length; i += 2)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+            return Ok(Math.Abs(sum));
+        }
     }
 }
