83cc9ec4a04b893e0c7457ced7b3647252870df3
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-27 23:34:52 +0300

    Add test for adding items in linked list data structure

--- BISolutionsTests/LinkedListTests.cs
+++ BISolutionsTests/LinkedListTests.cs
@@ -8,5 +8,16 @@ namespace BISolutionsTests
 {
     public class LinkedListTests
     {
+        [Fact]
+        public void Add_ShouldAddValuesCorrectly()
+        {
+            LinkedList.LinkedList<int> list = new LinkedList.LinkedList<int>();
+
+            list.Add(5);
+            list.Add(10);
+            list.Add(15);
+
+            Assert.Equal(new int[] { 5, 10, 15 }, list.GetValues().ToArray());
+        }
     }
 }
